ee1bd235098c138ccf4b5b39c6521618df020a71
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-07-14 20:20:25 +0100

    2 peer connection

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+app.get("/peer", (req, res) => {
+    res.render("peer")
+})
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('offer', (offer) => {
+        console.log(offer)
+        io.emit('offer', offer);
+       
+    })
+    socket.on('answer', (answer) => {
+        console.log(answer)
+        io.emit('answer', answer);
+       
+    })
+    
+})
+server.listen(4000, () => console.log("listening on port 4000"))
